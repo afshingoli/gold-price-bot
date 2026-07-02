792567ff5729f53a87536190e85cdc41fb4e1d6b
afshingoli <iafshingoli@gmail.com>
2026-07-02 15:53:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,29 @@ import requests
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-url = "http://et.tala.ir/webservice/haghanigold.com/6397dbw8333f095bb55cd539f865a994"
+API_URL = "http://et.tala.ir/webservice/haghanigold.com/6397dbw8333f095bb55cd539f865a994"
 
-data = requests.get(url).json()
+# دریافت اطلاعات
+data = requests.get(API_URL).json()
 
 price = data["geram18"]["value"]
 time = data["serverTime"]
+
+# خواندن آخرین قیمت
+try:
+    with open("last_price.txt", "r") as f:
+        last_price = f.read().strip()
+except:
+    last_price = ""
+
+# اگر قیمت تغییر نکرد، هیچ کاری نکن
+if last_price == str(price):
+    print("Price not changed.")
+    exit()
+
+# ذخیره قیمت جدید
+with open("last_price.txt", "w") as f:
+    f.write(str(price))
 
 price_text = f"{price:,}"
 
