c5f901315889dba93a4007b02fb6e7a496e4e98b
afshingoli <iafshingoli@gmail.com>
2026-07-02 13:48:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-ِimport requestsِِ
+import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.tala.ir/price/18k"
@@ -15,4 +15,4 @@ text = soup.get_text()
 
 index = text.find("آخرین قیمت")
 
-print(text[index:index+1000])
+print(text[index:index+1000])ُ
