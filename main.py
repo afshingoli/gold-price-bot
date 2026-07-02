bb46526a727ee383ec41926ee1f2aa3b60cfbb0f
afshingoli <iafshingoli@gmail.com>
2026-07-02 11:12:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import requests
 
+url = "https://www.tala.ir/price/18k"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url, headers=headers)
+
+print("Status Code:", response.status_code)
+print(response.text[:1000])
